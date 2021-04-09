5cd2db428ff47c4f629b0000a9f2f8db23f0acbe
Rendy <lierendysetiawan3232@gmail.com>
2021-04-09 14:40:04 +0800

    Heroku Try

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./routes');
+const path = require('path')
 
+app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(session({
