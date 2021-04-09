42bff20b49647e2722cf29253dde360b853b9153
Rendy <lierendysetiawan3232@gmail.com>
2021-04-09 13:52:03 +0800

    Heroku - Deploy 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const port = 3000;
+const port = prcess.env.PORT || 3000;
 const router = require('./routes');
 
 app.set('view engine', 'ejs');
